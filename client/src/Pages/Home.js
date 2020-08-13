050ab3e6d86144022ade7cc3982dcf9a41c2500b
Hy-An Exequiel <nathan.hyan2048@gmail.com>
2020-08-13 13:18:07 -0300

    Home mockup

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,11 +1,33 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { Container, Row, Col, Button } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLink } from '@fortawesome/free-solid-svg-icons'
 
 export default function Home() {
 
+    const rowStyle = {
+        height: "calc(100vh - 56px)"
+    }
+
+
     return (
         <Container>
-            <h1 className="display-4">This is stuff</h1>
+            <Row style={rowStyle} className='align-items-center'>
+                <Col>
+                    <h1>The place for your invoice needs! 📃</h1>
+
+                    <p className="lead">Stop using paper when handing invoices to clients. Send the invoice, the client signs it with their phone and everyone involved receives a copy via e-mail! Is that easy!. </p>
+
+                    <Link to='/createInvoice'>
+                        <Button>
+                            <FontAwesomeIcon icon={faLink} /> Start Now
+                        </Button>
+                    </Link>
+                </Col>
+
+                <Col className='text-right'><h3>this is page... welcome</h3></Col>
+            </Row>
         </Container>
     )
 }
