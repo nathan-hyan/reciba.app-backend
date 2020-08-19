d537d36d6a48262cb7c191476c5843c84c3f6b7c
Hy-An Exequiel <nathan.hyan2048@gmail.com>
2020-08-19 16:01:19 -0300

    Migrated JS to TSX

---
+++ client/src/Context/UserContext.tsx
@@ -0,0 +1,33 @@
+import React, { createContext, useState } from 'react';
+import Axios from 'axios';
+
+type ContextProps = {
+	isLoggedIn: boolean;
+	level: number;
+	token: string;
+	name: string;
+	setUserData: any;
+};
+
+export const UserContext = createContext<Partial<ContextProps>>({});
+
+const UserContextProvider = (props) => {
+	const storedToken = localStorage.getItem('bill-token');
+	const [ userData, setUserData ] = useState({
+		isLoggedIn: false,
+		level: 9,
+		token: '',
+		name: '',
+	});
+
+	if (storedToken && !userData.isLoggedIn) {
+		Axios.post('/api/user/loggedInUser', { storedToken }).then((response) => {
+			localStorage.setItem('bill-token', response.data.data.token);
+			setUserData({ isLoggedIn: true, level: 9, token: response.data.data.token, name: response.data.data.name });
+		});
+	}
+
+	return <UserContext.Provider value={{ ...userData, setUserData }}>{props.children}</UserContext.Provider>;
+};
+
+export default UserContextProvider;
