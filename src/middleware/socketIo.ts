0d57530b790f75e742388dc03b1cdaac0e0ac088
Hy-An Exequiel <nathan.hyan2048@gmail.com>
2020-11-28 15:02:37 -0300

    Changed misleading logs in console

--- src/middleware/socketIo.ts
+++ src/middleware/socketIo.ts
@@ -1,30 +1,31 @@
-import { Socket } from "socket.io";
+import { Socket } from 'socket.io';
 
 const socket = (io: any) => {
-  io.on("connection", (socket: Socket) => {
+  io.on('connection', (socket: Socket) => {
     console.log(`>> SOCKET || ${socket.id} just connected`);
 
-    socket.on("join", (room: string) => {
-      console.log(`>> SOCKET || Connected to ${room}`);
+    socket.on('join', (room: string) => {
+      console.log(`>> SOCKET || ${socket.id} connected to ${room}`);
+      console.log('Current rooms:', socket.rooms);
       socket.join(room);
 
-      socket.on("disconnect", () => {
-        console.log(`>> SOCKET || ${socket.id} disconnected`);
+      socket.on('disconnect', (data) => {
+        console.log(`>> SOCKET || ${socket.id} disconnected`, data);
       });
 
-      socket.on("close", () => {
-        io.to(room).emit("close", false);
-        console.log(`>> SOCKET || ${socket.id} disconnected`);
+      socket.on('close', () => {
+        io.to(room).emit('close', false);
+        console.log(`>> SOCKET || closed the QR Modal`);
       });
 
-      socket.on("pdf", (file: string) => {
+      socket.on('pdf', (file: string) => {
         console.log(`>> SOCKET || ${socket.id} send a file`);
-        io.to(room).emit("pdf", file);
+        io.to(room).emit('pdf', file);
       });
 
-      socket.on("sign", (signature: string) => {
+      socket.on('sign', (signature: string) => {
         console.log(`>> SOCKET || ${socket.id} signed a bill`);
-        io.to(room).emit("sign", signature);
+        io.to(room).emit('sign', signature);
       });
     });
   });
